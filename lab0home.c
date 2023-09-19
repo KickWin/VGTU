66674683c95324e7177bfa4531b954ce575f97b4
Крысанов Ярослав <kickwinyar@gmail.com>
2023-09-19 23:27:22 +0300

    lab0

---
+++ lab0home.c
@@ -0,0 +1,14 @@
+#include <locale.h>
+#include <stdio.h>
+
+void main() {
+
+setlocale(LC_ALL, "RUS");
+puts ("        __    "); 
+puts ("       (  )   "); 
+puts ("    \\  (  )  /"); 
+puts ("     \\  ||  / "); 
+puts ("      \\ || /  "); 
+puts ("       \\||/   ");  
+
+}
